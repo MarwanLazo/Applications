b765951cc7d51b867b81fcef857f8f1b427c3283
Marwan <eng.marawan1988@yahoo.com>
2016-04-21 16:27:49 +0200

    hibernate audit listiner conditional registry

---
+++ src/com/sprapp/audit/envers/listiner/SprEnversPostDeleteEventListenerImpl.java
@@ -0,0 +1,19 @@
+package com.sprapp.audit.envers.listiner;
+
+import org.hibernate.envers.configuration.spi.AuditConfiguration;
+import org.hibernate.envers.event.spi.EnversPostDeleteEventListenerImpl;
+import org.hibernate.event.spi.PostDeleteEvent;
+
+public class SprEnversPostDeleteEventListenerImpl extends EnversPostDeleteEventListenerImpl {
+	private static final long serialVersionUID = 1L;
+
+	public SprEnversPostDeleteEventListenerImpl(AuditConfiguration enversConfiguration) {
+		super(enversConfiguration);
+	}
+
+	@Override
+	public void onPostDelete(PostDeleteEvent event) {
+		super.onPostDelete(event);
+	}
+
+}
